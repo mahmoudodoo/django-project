347704ad43c32d8bd92150a72402fa058f1d2ee6
mahmoudodoo <mahmoudodoo11@gmail.com>
2020-11-24 15:02:31 +0200

    Add the content of xo and connect4 games ,body, javascript, and styles

--- simpleWebSite/basic_app/views/connect4_view.py
+++ simpleWebSite/basic_app/views/connect4_view.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
-
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
 
 def connect4(request):
+    if request.method == 'POST':
+        return HttpResponseRedirect(reverse('connect4'))
     return render(request,'connect4.html',{})
+    
